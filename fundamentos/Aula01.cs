94862802a3a78e8c190c544e2e7ce0b9df1f7d95
wellsj <wellserafimj@gmail.com>
2026-03-28 17:57:17 +0000

    2° commit Aula01

---
+++ fundamentos/Aula01.cs
@@ -0,0 +1,44 @@
+namespace fundamentos01;
+
+public class Aula01
+{
+    public void ExecutarAula()
+    {
+        // Início
+        this.ExibirCabecalho();
+        
+
+
+
+
+
+
+
+        // Fim  
+        this.ResumoAula();
+
+    }
+
+
+    private void ExibirCabecalho()
+    {
+        Console.WriteLine("=======================================");
+        Console.WriteLine("  Aula 01 - fundamentos de algoritimia ");
+        Console.WriteLine("=======================================");
+        Console.WriteLine();
+    }
+
+    private void ResumoAula()
+    {
+        Console.WriteLine();
+        Console.WriteLine("=======================================");
+        Console.WriteLine(" Fim da aula - resumo ");
+        Console.WriteLine("=======================================");
+        Console.WriteLine(" O que é algoritimia");
+        Console.WriteLine(" Variáveis e tipos de dados");
+        Console.WriteLine(" Operadores aritiméticos, comparação e lógicas");
+        Console.WriteLine(" estrutura de controle básicos");
+    }
+
+
+} // Fim da aula01
--- fundamentos/Program.cs
+++ fundamentos/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using fundamentos01;
+
+namespace Fundamentos01;
+
+    public class aula01
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Início da aula01 \n");
+            Aula01 aula01 = new Aula01();
+            aula01.ExecutarAula();
+
+        }
+        
+
+    } // fim da class
+    
+
